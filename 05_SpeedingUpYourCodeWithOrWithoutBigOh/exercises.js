8924b24280f7b341f37f02e450d3b7ebefb74016
SimProch <mavian@email.cz>
2021-12-31 16:00:36 +0100

    Added exercises

---
+++ 05_SpeedingUpYourCodeWithOrWithoutBigOh/exercises.js
@@ -0,0 +1,40 @@
+/**
+ * Big Oh for 4N + 16 operations
+ */
+function one() {
+    console.log("O(N)");
+}
+
+/**
+ * Big Oh for 2N^2
+ */
+function two() {
+    console.log("O(N^2)");
+}
+
+/**
+ * Big Oh for 2 identical loops over N elements
+ */
+function three() {
+    console.log("O(N)")
+}
+
+/**
+ * Big Oh for 1 loop with 3 operations over N elements
+ */
+function four(array) {
+    console.log("O(N)")
+}
+
+/**
+ * Big Oh for one loop in which another is nested
+ */
+function five() {
+    console.log("O(N^2)")
+}
+
+one();
+two();
+three();
+four();
+five();
