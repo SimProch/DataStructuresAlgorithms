f1098f9fbf180706bd01d575e5db68cb12c759ff
SimProch <mavian@email.cz>
2022-01-01 20:14:38 +0100

    Finished chapter 6

--- 06_OptimizingForOptimisticScenarios/exercises.js
+++ 06_OptimizingForOptimisticScenarios/exercises.js
@@ -1,1 +1,33 @@
 
+/**
+ * Big Oh for 3N^2 + 2N + 1
+ */
+ function one() {
+    console.log("O(N^2)");
+}
+
+/**
+ * Big Oh for N + log N
+ */
+function two() {
+    console.log("O(N)");
+}
+
+/**
+ * Big Oh for searching 2 numbers that add up to 10 (nested for)
+ */
+function three() {
+    console.log("O(N^2) Worst case, O(1) best case (early return)")
+}
+
+/**
+ * Big Oh for finding X in string
+ */
+function four(array) {
+    console.log("O(N) (Early break if foundX is true)")
+}
+
+one();
+two();
+three();
+four();
